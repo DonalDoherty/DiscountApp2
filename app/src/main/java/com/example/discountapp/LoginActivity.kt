b21d8aa0a3b2eb25902559784ffd0113dc0d0b3e
DonalDoherty <dohertydonal98@gmail.com>
2020-02-14 18:19:18 +0000

    Removed unnecessary code

--- app/src/main/java/com/example/discountapp/LoginActivity.kt
+++ app/src/main/java/com/example/discountapp/LoginActivity.kt
@@ -1,15 +1,12 @@
 package com.example.discountapp
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
-import kotlinx.android.synthetic.main.activity_search.*
-
-class SearchActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search)
+        setContentView(R.layout.activity_login)
     }
-
 }
