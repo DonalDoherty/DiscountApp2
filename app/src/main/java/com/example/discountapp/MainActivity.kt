ea92e00e59fbc6dcf5d38db65c7994e684cc4e33
Donal Doherty <dohertydonal98@gmail.com>
2020-05-08 18:45:57 +0100

    Added Recents list

--- app/src/main/java/com/example/discountapp/MainActivity.kt
+++ app/src/main/java/com/example/discountapp/MainActivity.kt
@@ -3,14 +3,35 @@ package com.example.discountapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.discountapp.models.Post
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.toObject
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_profile.*
 
 class MainActivity : AppCompatActivity() {
     private val mAuth = FirebaseAuth.getInstance()
+    private val db = FirebaseFirestore.getInstance()
+    private var adaptedPosts = mutableListOf<Post?>()
+    private var postAdapter = PostAdapter(adaptedPosts as List<Post>)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var mLayoutManager = LinearLayoutManager(this)
+        recents_list_main.layoutManager = mLayoutManager
+        recents_list_main.itemAnimator = DefaultItemAnimator()
+        recents_list_main.adapter = postAdapter
+        db.collection("posts").get().addOnSuccessListener{documents ->
+            for (document in documents) {
+                var postList = mutableListOf<String>()
+                postList.add(document.id)
+                importPosts(postList)
+            }
+
+        }
         //check if user is logged in, if not, send to login screen
         if (mAuth.currentUser == null) {
             val intent = Intent(this, LoginActivity::class.java)
@@ -29,6 +50,20 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, CreateActivity::class.java )
             startActivity(intent)
         }
+
+
+    }
+    fun importPosts(postList: List<String>){
+        for (id in postList)
+            db.collection("posts").document(id).get().addOnSuccessListener { documentSnapshot ->
+                var post = documentSnapshot.toObject<Post>()
+                if (post != null) {
+                    post.id = id
+                }
+                adaptedPosts.add(post)
+                this.adaptedPosts = adaptedPosts
+                postAdapter.notifyDataSetChanged()
+            }
     }
 }
 
